cf96cf206785cc34e2e039708c28f9b2ea7d9361
SeaCuts <125990960+SeaCutter@users.noreply.github.com>
2023-03-07 21:57:29 +0530

    Update list.js

--- list.js
+++ list.js
@@ -1,3 +1,5 @@
+const cartItems = [];
+
 fetch('items.csv')
   .then(response => response.text())
   .then(data => {
@@ -18,6 +20,16 @@ fetch('items.csv')
       const html = renderItem(item);
       chickenContainer.innerHTML += html;
     });
+
+    const addButtons = document.querySelectorAll('.add-to-cart');
+    addButtons.forEach(button => {
+      button.addEventListener('click', () => {
+        const itemId = button.dataset.itemId;
+        const item = items.find(item => item.id === itemId);
+        cartItems.push(item);
+        renderCart();
+      });
+    });
   });
 
 function renderItem(item) {
@@ -26,9 +38,31 @@ function renderItem(item) {
       <img src="${item.image}">
       <h3>${item.name}</h3>
       <p>${item.price}</p>
-      <button class="add-to-cart" data-name="${item.name}" data-price="${item.price}">Add to Cart</button>
+      <button class="add-to-cart" data-item-id="${item.id}">Add to Cart</button>
     </div>
   `;
 }
 
+function renderCart() {
+  const cartItemsList = document.getElementById('cart-items');
+  cartItemsList.innerHTML = '';
+  cartItems.forEach(item => {
+    const html = `
+      <li>
+        <span>${item.name}</span>
+        <span>${item.price}</span>
+      </li>
+    `;
+    cartItemsList.innerHTML += html;
+  });
+
+  const cartTotal = cartItems.reduce((total, item) => total + parseFloat(item.price), 0);
+  const cartTotalElement = document.getElementById('cart-total');
+  cartTotalElement.textContent = `$${cartTotal.toFixed(2)}`;
+}
+
+const checkoutButton = document.getElementById('checkout-button');
+checkoutButton.addEventListener('click', () => {
+  alert(`Total: $${cartItems.reduce((total, item) => total + parseFloat(item.price), 0).toFixed(2)}`);
+});
 
